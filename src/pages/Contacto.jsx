7d928c2142660bf39cb2847c73b5180dc3a42f01
MAILYN707 <mailyn.delgado@ucr.ac.cr>
2025-04-06 23:21:39 -0600

    chore: prueba

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,6 +1,15 @@
-export function Contacto(){
+export function Contacto() {
     return (
-        <div>Pagina de Contacto</div>
+        <div>
+            <h1 className='text-center font-bold font-playfair text-[45px] eading-none text-[#07484A] mt-12'>¿Quiénes Somos?</h1>
+            <div className="flex justify-center w-[938px] h-[212px] top-[173px] left-[564px]">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate suscipit ipsum repellendus veniam non amet expedita odit ad vel eum? Quos minima eos doloribus reiciendis praesentium, laboriosam ipsa est corporis.
+
+                </p>
+            </div>
+
+        </div>
 
     )
 }
