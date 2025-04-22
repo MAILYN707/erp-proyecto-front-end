e7f8decf56b589c1c0d8a6003a74aaf7f7ad3281
MAILYN707 <mailyn.delgado@ucr.ac.cr>
2025-04-22 15:16:52 -0600

    feat: implementacion de filtrado por categorias y búsqueda

--- src/components/Productos/GridProductos.jsx
+++ src/components/Productos/GridProductos.jsx
@@ -2,29 +2,30 @@ import { CardProductos } from './CardProductos';
 import { useEffect, useState } from 'react';
 import { axiosClient } from '@services/axiosClient'
 
-export function GridProductos() {
-
+export function GridProductos({ filtroBusqueda, categoriaSeleccionada }) {
   const [productos, setProductos] = useState([]);
-  const [loading, setLoading] = useState(true);
+
 
   useEffect(() => {
     axiosClient.get('/productos')
-      .then(res => {
-        if (res.data && Array.isArray(res.data.data)) {
-          setProductos(res.data.data);
-        } else {
-          console.warn('Formato inesperado de productos:', res.data);
-        }
-      })
-      .catch(err => console.error('Error al obtener productos:', err))
-      .finally(() => setLoading(false));
+      .then(res => setProductos(res.data.data))
+      .catch(err => console.error('Error al obtener productos:', err));
   }, []);
 
+  const productosFiltrados = productos.filter((p) => {
+    const coincideCategoria =
+      categoriaSeleccionada === '' || p.id_categoriaProducto === categoriaSeleccionada;
+    const coincideBusqueda =
+      filtroBusqueda.trim() === '' || p.nombre.toLowerCase().includes(filtroBusqueda.toLowerCase());
+  
+    return coincideCategoria && coincideBusqueda;
+  });
+  
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 px-4 pb-8">
-      {productos.map((p) => (
-        <CardProductos key={p.id_producto} producto={p} />
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 px-4 pb-8 ml-10">
+      {productosFiltrados.map((producto) => (
+        <CardProductos key={producto.id_producto} producto={producto} />
       ))}
     </div>
 
