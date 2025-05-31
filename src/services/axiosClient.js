92137b57e4b34d7486ef15b25a1cac158d3cf964
Danier Jordany Morales Castro <danier.morales@ucr.ac.cr>
2025-05-31 16:21:26 -0600

    Funcion de Facturacion parcialmente completado

--- src/services/axiosClient.js
+++ src/services/axiosClient.js
@@ -3,10 +3,14 @@ import axios from 'axios';
 const token = localStorage.getItem('token');
 
 export const axiosClient = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'http://Localhost:8000/api',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
   },
   withCredentials: false,
 });
+
+if (token) {
+  axiosClient.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
