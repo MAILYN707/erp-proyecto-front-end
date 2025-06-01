19cd1a876912dfa23f245358af0e98c6428c5dbc
Mailyn <mailyn.delgado@ucr.ac.cr>
2025-05-31 18:56:39 -0600

    Revert "Merge branch 'development' into feature/usuario-administrador"

--- src/components/routes/PublicRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import { useUser } from '../UserContext';
-
-export function PublicRoute({ children }) {
-  const { usuario } = useUser();
-  return !usuario ? children : <Navigate to="/" />;
-}
